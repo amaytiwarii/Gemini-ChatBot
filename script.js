35b175b3604bd55bf57a567746932c2c436c8c18
Amay Tiwari <tiwariamay7@gmail.com>
2025-12-07 15:21:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const fileInput = promptForm.querySelector("#file-input");
 const fileUploadWrapper = promptForm.querySelector(".file-upload-wrapper");
 const themeToggleBtn = document.querySelector("#theme-toggle-btn");
 
-const API_KEY = "AIzaSyDuyVdIzjJBqXnVWM7iELQcdmggNexJ6AY";
-const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
+const API_KEY = "YOUR API KEY";
+const API_URL = `YOUR API URL`;
 let controller, typingInterval;
 const chatHistory = [ {
   role: "user",
